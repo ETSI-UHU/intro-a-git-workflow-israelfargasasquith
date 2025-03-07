004d1e92b664111c26a0e36552ad72e7a9204129
JOSE <JOSE@DESKTOP-ABCVK4U>
2025-03-07 12:39:04 +0100

    Refactorización del método "operacionResta".

--- src/pkg/Calculadora.java
+++ src/pkg/Calculadora.java
@@ -26,8 +26,7 @@ public class Calculadora {
                 System.out.println("Resultado: " + resultado);
                 break;
             case '-':
-                resultado = num1 - num2;
-                System.out.println("Resultado: " + resultado);
+			operacionResta(num1, num2);
                 break;
             case '*':
                 resultado = num1 * num2;
@@ -47,4 +46,10 @@ public class Calculadora {
 
         scanner.close();
     }
+
+	public static void operacionResta(double num1, double num2) {
+		double resultado;
+		resultado = num1 - num2;
+		System.out.println("Resultado: " + resultado);
+	}
 }
